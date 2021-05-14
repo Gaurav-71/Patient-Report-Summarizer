a3c438499c198fa556a241e06ab23754ae9b733e
Gaurav V <gv71.msrit@gmail.com>
2021-05-14 13:46:10 +0530

    finished text summarization

--- summarizer.py
+++ summarizer.py
@@ -1,203 +1,201 @@
-#Expanding Contractions for frequently used shortforms
+# Expanding Contractions for frequently used shortforms
+import html
+from sys import path
+import numpy as np
+import re
+import networkx
+from gensim.summarization import summarize, keywords
+from nltk.corpus import wordnet as wn
+from pattern3.en import tag
+import unicodedata
+from nltk.stem import WordNetLemmatizer
+import string
+import nltk
+from scipy.sparse.linalg import svds
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
 CONTRACTION_MAP = {
-"ain't": "is not",
-"aren't": "are not",
-"can't": "cannot",
-"can't've": "cannot have",
-"'cause": "because",
-"could've": "could have",
-"couldn't": "could not",
-"couldn't've": "could not have",
-"didn't": "did not",
-"doesn't": "does not",
-"don't": "do not",
-"hadn't": "had not",
-"hadn't've": "had not have",
-"hasn't": "has not",
-"haven't": "have not",
-"he'd": "he would",
-"he'd've": "he would have",
-"he'll": "he will",
-"he'll've": "he he will have",
-"he's": "he is",
-"how'd": "how did",
-"how'd'y": "how do you",
-"how'll": "how will",
-"how's": "how is",
-"I'd": "I would",
-"I'd've": "I would have",
-"I'll": "I will",
-"I'll've": "I will have",
-"I'm": "I am",
-"I've": "I have",
-"i'd": "i would",
-"i'd've": "i would have",
-"i'll": "i will",
-"i'll've": "i will have",
-"i'm": "i am",
-"i've": "i have",
-"isn't": "is not",
-"it'd": "it would",
-"it'd've": "it would have",
-"it'll": "it will",
-"it'll've": "it will have",
-"it's": "it is",
-"let's": "let us",
-"ma'am": "madam",
-"mayn't": "may not",
-"might've": "might have",
-"mightn't": "might not",
-"mightn't've": "might not have",
-"must've": "must have",
-"mustn't": "must not",
-"mustn't've": "must not have",
-"needn't": "need not",
-"needn't've": "need not have",
-"o'clock": "of the clock",
-"oughtn't": "ought not",
-"oughtn't've": "ought not have",
-"shan't": "shall not",
-"sha'n't": "shall not",
-"shan't've": "shall not have",
-"she'd": "she would",
-"she'd've": "she would have",
-"she'll": "she will",
-"she'll've": "she will have",
-"she's": "she is",
-"should've": "should have",
-"shouldn't": "should not",
-"shouldn't've": "should not have",
-"so've": "so have",
-"so's": "so as",
-"that'd": "that would",
-"that'd've": "that would have",
-"that's": "that is",
-"there'd": "there would",
-"there'd've": "there would have",
-"there's": "there is",
-"they'd": "they would",
-"they'd've": "they would have",
-"they'll": "they will",
-"they'll've": "they will have",
-"they're": "they are",
-"they've": "they have",
-"to've": "to have",
-"wasn't": "was not",
-"we'd": "we would",
-"we'd've": "we would have",
-"we'll": "we will",
-"we'll've": "we will have",
-"we're": "we are",
-"we've": "we have",
-"weren't": "were not",
-"what'll": "what will",
-"what'll've": "what will have",
-"what're": "what are",
-"what's": "what is",
-"what've": "what have",
-"when's": "when is",
-"when've": "when have",
-"where'd": "where did",
-"where's": "where is",
-"where've": "where have",
-"who'll": "who will",
-"who'll've": "who will have",
-"who's": "who is",
-"who've": "who have",
-"why's": "why is",
-"why've": "why have",
-"will've": "will have",
-"won't": "will not",
-"won't've": "will not have",
-"would've": "would have",
-"wouldn't": "would not",
-"wouldn't've": "would not have",
-"y'all": "you all",
-"y'all'd": "you all would",
-"y'all'd've": "you all would have",
-"y'all're": "you all are",
-"y'all've": "you all have",
-"you'd": "you would",
-"you'd've": "you would have",
-"you'll": "you will",
-"you'll've": "you will have",
-"you're": "you are",
-"you've": "you have"
+    "ain't": "is not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "I'd": "I would",
+    "I'd've": "I would have",
+    "I'll": "I will",
+    "I'll've": "I will have",
+    "I'm": "I am",
+    "I've": "I have",
+    "i'd": "i would",
+    "i'd've": "i would have",
+    "i'll": "i will",
+    "i'll've": "i will have",
+    "i'm": "i am",
+    "i've": "i have",
+    "isn't": "is not",
+    "it'd": "it would",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so as",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there would",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we would",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you would",
+    "you'd've": "you would have",
+    "you'll": "you will",
+    "you'll've": "you will have",
+    "you're": "you are",
+    "you've": "you have"
 }
 
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-
 def build_feature_matrix(documents, feature_type='frequency'):
-
-    feature_type = feature_type.lower().strip()  
-    
+    feature_type = feature_type.lower().strip()
     if feature_type == 'binary':
-        vectorizer = CountVectorizer(binary=True, min_df=1, 
-                                     ngram_range=(1, 1))
+        vectorizer = CountVectorizer(binary=True, min_df=1, ngram_range=(1, 1))
     elif feature_type == 'frequency':
-        vectorizer = CountVectorizer(binary=False, min_df=1, 
-                                     ngram_range=(1, 1))
+        vectorizer = CountVectorizer(
+            binary=False, min_df=1, ngram_range=(1, 1))
     elif feature_type == 'tfidf':
-        vectorizer = TfidfVectorizer(min_df=1, 
-                                     ngram_range=(1, 1))
+        vectorizer = TfidfVectorizer(min_df=1, ngram_range=(1, 1))
     else:
-        raise Exception("Wrong feature type entered. Possible values: 'binary', 'frequency', 'tfidf'")
-
+        raise Exception(
+            "Wrong feature type entered. Possible values: 'binary', 'frequency', 'tfidf'")
     feature_matrix = vectorizer.fit_transform(documents).astype(float)
-    
     return vectorizer, feature_matrix
 
 
-from scipy.sparse.linalg import svds
-    
 def low_rank_svd(matrix, singular_count=2):
-    
     u, s, vt = svds(matrix, k=singular_count)
     return u, s, vt
 
-import re
-import nltk
-import string
-from nltk.stem import WordNetLemmatizer
-from html.parser import HTMLParser
-import unicodedata
 
 stopword_list = nltk.corpus.stopwords.words('english')
 wnl = WordNetLemmatizer()
-html_parser = HTMLParser()
 
 # tokenize the report into tokens
+
+
 def tokenize_text(text):
-    tokens = nltk.word_tokenize(text) 
+    tokens = nltk.word_tokenize(text)
     tokens = [token.strip() for token in tokens]
     return tokens
 
 # Match the shortforms used in the report by doctors and replace them with the correct words
+
+
 def expand_contractions(text, contraction_mapping):
-    
-    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), 
-                                      flags=re.IGNORECASE|re.DOTALL)
+    contractions_pattern = re.compile('({})'.format(
+        '|'.join(contraction_mapping.keys())), flags=re.IGNORECASE | re.DOTALL)
+
     def expand_match(contraction):
         match = contraction.group(0)
         first_char = match[0]
         expanded_contraction = contraction_mapping.get(match)\
-                                if contraction_mapping.get(match)\
-                                else contraction_mapping.get(match.lower())                       
+            if contraction_mapping.get(match)\
+            else contraction_mapping.get(match.lower())
         expanded_contraction = first_char+expanded_contraction[1:]
         return expanded_contraction
-        
     expanded_text = contractions_pattern.sub(expand_match, text)
     expanded_text = re.sub("'", "", expanded_text)
     return expanded_text
-    
-
-from pattern.en import tag
-from nltk.corpus import wordnet as wn
 
 # Annotate text tokens with POS tags
+
+
 def pos_tag_text(text):
-    
     def penn_to_wn_tags(pos_tag):
         if pos_tag.startswith('J'):
             return wn.ADJ
@@ -209,51 +207,55 @@ def pos_tag_text(text):
             return wn.ADV
         else:
             return None
-    
     tagged_text = tag(text)
     tagged_lower_text = [(word.lower(), penn_to_wn_tags(pos_tag))
-                         for word, pos_tag in
-                         tagged_text]
+                         for word, pos_tag in tagged_text]
     return tagged_lower_text
-    
-# lemmatize text based on POS tags    
+
+# lemmatize text based on POS tags
+
+
 def lemmatize_text(text):
-    
+
     pos_tagged_text = pos_tag_text(text)
-    lemmatized_tokens = [wnl.lemmatize(word, pos_tag) if pos_tag
-                         else word                     
-                         for word, pos_tag in pos_tagged_text]
+    lemmatized_tokens = [wnl.lemmatize(
+        word, pos_tag) if pos_tag else word for word, pos_tag in pos_tagged_text]
     lemmatized_text = ' '.join(lemmatized_tokens)
     return lemmatized_text
-    
+
 
 # to eliminate special caracters from report
 def remove_special_characters(text):
     tokens = tokenize_text(text)
-    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))     #string.punctuation = !"#$%&'()*+, -./:;<=>?@[\]^_`{|}~
-    filtered_tokens = filter(None, [pattern.sub(' ', token) for token in tokens])
+    # string.punctuation = !"#$%&'()*+, -./:;<=>?@[\]^_`{|}~
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(
+        None, [pattern.sub(' ', token) for token in tokens])
     filtered_text = ' '.join(filtered_tokens)
     return filtered_text
-    
+
 # to eliminate stop words which do not provide any useful info
+
+
 def remove_stopwords(text):
     tokens = tokenize_text(text)
     filtered_tokens = [token for token in tokens if token not in stopword_list]
-    filtered_text = ' '.join(filtered_tokens)    
+    filtered_text = ' '.join(filtered_tokens)
     return filtered_text
 
-
 # to remove any html related syntax
+
+
 def unescape_html(parser, text):
-    
     return parser.unescape(text)
 
-#normalization of text
-def normalize_corpus(corpus, lemmatize=True,tokenize=False):
-    
-    normalized_corpus = []  
+# normalization of text
+
+
+def normalize_corpus(corpus, lemmatize=True, tokenize=False):
+    normalized_corpus = []
     for text in corpus:
-        text = html_parser.unescape(text)
+        text = html.unescape(text)
         text = expand_contractions(text, CONTRACTION_MAP)
         if lemmatize:
             text = lemmatize_text(text)
@@ -266,10 +268,7 @@ def normalize_corpus(corpus, lemmatize=True,tokenize=False):
             normalized_corpus.append(text)
         else:
             normalized_corpus.append(text)
-        
-            
     return normalized_corpus
-
 
 
 # parse the document to check non ascii characters
@@ -286,79 +285,57 @@ def parse_document(document):
     sentences = [sentence.strip() for sentence in sentences]
     return sentences
 
-import numpy as np
-import docx2txt
-import glob
-from gensim.summarization import summarize, keywords
 
 def text_summarization_gensim(text, summary_ratio=0.5):
-    
     summary = summarize(text, split=True, ratio=summary_ratio)
     for sentence in summary:
-        print (sentence)
-    
-    
-def lsa_text_summarizer(documents, num_sentences=2,num_topics=2, feature_type='frequency',sv_threshold=0.5):
-                            
-    vec, dt_matrix = build_feature_matrix(documents, feature_type)
+        print(sentence)
 
+
+def lsa_text_summarizer(documents, sentences, num_sentences=2, num_topics=2, feature_type='frequency', sv_threshold=0.5):
+    vec, dt_matrix = build_feature_matrix(documents, feature_type)
     td_matrix = dt_matrix.transpose()
     td_matrix = td_matrix.multiply(td_matrix > 0)
-
-    u, s, vt = low_rank_svd(td_matrix, singular_count=num_topics)  
+    u, s, vt = low_rank_svd(td_matrix, singular_count=num_topics)
     min_sigma_value = max(s) * sv_threshold
     s[s < min_sigma_value] = 0
-    
     salience_scores = np.sqrt(np.dot(np.square(s), np.square(vt)))
     top_sentence_indices = salience_scores.argsort()[-num_sentences:][::-1]
     top_sentence_indices.sort()
-    
     for index in top_sentence_indices:
-        print (sentences[index])
-    
-    
-    
+        print(sentences[index])
 
-import networkx
 
-def textrank_text_summarizer(documents, num_sentences=2,
-                             feature_type='frequency'):
-    
-    vec, dt_matrix = build_feature_matrix(norm_sentences, 
-                                      feature_type='tfidf')
+def textrank_text_summarizer(documents, sentences, num_sentences=2, feature_type='frequency'):
+    vec, dt_matrix = build_feature_matrix(documents, feature_type='tfidf')
     similarity_matrix = (dt_matrix * dt_matrix.T)
-        
     similarity_graph = networkx.from_scipy_sparse_matrix(similarity_matrix)
-    scores = networkx.pagerank(similarity_graph)   
-    
-    ranked_sentences = sorted(((score, index) 
-                                for index, score 
-                                in scores.items()), 
-                              reverse=True)
-
-    top_sentence_indices = [ranked_sentences[index][1] 
+    scores = networkx.pagerank(similarity_graph)
+    ranked_sentences = sorted(((score, index)
+                               for index, score in scores.items()), reverse=True)
+    top_sentence_indices = [ranked_sentences[index][1]
                             for index in range(num_sentences)]
     top_sentence_indices.sort()
-    
+    finalResult = []
     for index in top_sentence_indices:
-        print (sentences[index])
+        finalResult.append(sentences[index])
+    return finalResult
 
-l = list(glob.glob(r"./static/Data*.txt"))
 
-x = 1
-for i in l:
-    my_text = docx2txt.process(i)
-    sentences = parse_document(my_text)
-    norm_sentences = normalize_corpus(sentences,lemmatize=False) 
-    print ("Total Sentences:", len(norm_sentences))
+def summarisefile(filename):
+    path = "./static/Data/"+filename + ".txt"
+    i = open(path)
+    my_text = i.read()
+    print(my_text)
+    summary_sentences = parse_document(my_text)
+    normalized_sentences = normalize_corpus(summary_sentences, lemmatize=False)
+    print("Total Sentences:", len(normalized_sentences))
+    print("---------text-rank summarization for document--------")
+    summaryArr = textrank_text_summarizer(
+        normalized_sentences, summary_sentences, num_sentences=3, feature_type='tfidf')
+    summary = " ".join(summaryArr)
+    print(summaryArr)
+    return summaryArr
 
-    """print ("\n---------lsa summarization for document" + str(x) + "--------")
-    lsa_text_summarizer(norm_sentences, num_sentences=1,
-                        num_topics=2, feature_type='frequency',
-                        sv_threshold=0.5)"""
 
-    print ("---------text-rank summarization for document" + str(x) + "--------")
-    textrank_text_summarizer(norm_sentences, num_sentences=1,
-                             feature_type='tfidf')
-    
-    x = x + 1
+summarisefile('Chadwick Boseman')
